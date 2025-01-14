b98e7769f83572b3521f1f6fd5958374784daf59
[cristianbovenzi]git config --global user.name [cristianbovenzi] <[cristianbovenzi@gmail.com]>
2025-01-14 21:18:00 +0100

    cercare elementi nel dom

---
+++ cercare-elementi-nel-dom/cercare-elementi-nel-dom.js
@@ -0,0 +1,23 @@
+// Uso querySelectorAll
+const paragrafi = document.querySelectorAll(".paragrafo");
+console.log(paragrafi);
+
+// Uso getElementsByClassName
+const paragrafiAlt = document.getElementsByClassName("paragrafo");
+console.log(paragrafiAlt);
+
+// Uso getElementById
+const elemento3 = document.getElementById("elemento-3");
+console.log(elemento3);
+
+// Uso querySelector
+const elemento3Alt = document.querySelector("#elemento-3");
+console.log(elemento3Alt);
+
+// Uso querySelectorAll
+const titoliH2 = document.querySelectorAll("h2");
+console.log(titoliH2);
+
+// Uso getElementsByTagName
+const titoliH2Alt = document.getElementsByTagName("h2");
+console.log(titoliH2Alt);
