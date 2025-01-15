274842816da699e29b6e79e7be0e741f753acd86
[cristianbovenzi]git config --global user.name [cristianbovenzi] <[cristianbovenzi@gmail.com]>
2025-01-15 13:13:13 +0100

    aggiornato spostando altri metodi js in fondo alla pagina

--- cercare-elementi-nel-dom/cercare-elementi-nel-dom.js
+++ cercare-elementi-nel-dom/cercare-elementi-nel-dom.js
@@ -1,23 +1,24 @@
-// Uso querySelectorAll
-const paragrafi = document.querySelectorAll(".paragrafo");
+// Uso getElementsByClassName x cercare tutti gli elementi con classe paragrafo
+const paragrafi = document.getElementsByClassName("paragrafo");
 console.log(paragrafi);
 
-// Uso getElementsByClassName
-const paragrafiAlt = document.getElementsByClassName("paragrafo");
-console.log(paragrafiAlt);
-
-// Uso getElementById
+// Uso getElementById x cercare l'elemento con id "elemento-3"
 const elemento3 = document.getElementById("elemento-3");
 console.log(elemento3);
 
-// Uso querySelector
-const elemento3Alt = document.querySelector("#elemento-3");
-console.log(elemento3Alt);
-
-// Uso querySelectorAll
+// Uso querySelectorAll x cercare tutti gli elementi h2 della pagina
 const titoliH2 = document.querySelectorAll("h2");
 console.log(titoliH2);
 
+// altri metodi
+// Uso querySelectorAll
+// const paragrafi = document.querySelectorAll(".paragrafo");
+// console.log(paragrafi);
+
+// Uso querySelector
+// const elemento3 = document.querySelector("#elemento-3");
+// console.log(elemento3);
+
 // Uso getElementsByTagName
-const titoliH2Alt = document.getElementsByTagName("h2");
-console.log(titoliH2Alt);
+// const titoliH2 = document.getElementsByTagName("h2");
+// console.log(titoliH2);
